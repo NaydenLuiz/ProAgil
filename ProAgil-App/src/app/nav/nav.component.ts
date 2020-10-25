ce1531a60358a1abb57e05d3373c125922c49a9b
Nayden Luiz dos Santos Cruz <nayden.luiz@gmail.com>
2020-10-25 02:15:51 -0300

    Alteracao layout dashboard

--- ProAgil-App/src/app/nav/nav.component.ts
+++ ProAgil-App/src/app/nav/nav.component.ts
@@ -18,6 +18,9 @@ export class NavComponent implements OnInit {
   {
 
   }
+  showMenu(){
+    return this.router.url !== '/user/login';
+  }
   entrar(){
     this.router.navigate(['/user/login']);
   }
